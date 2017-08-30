ef49a2cb63d6be85250c04eede82dea4c7e4c37a
linjialiang1234 <linjialiang86@163.com>
2017-08-30 14:24:07 +0800

    exercise30

---
+++ week-02/day-03/exercise-30.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// - Create a variable named `aj`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Reverse the order of the elements in `aj`
+// 		- do it with the built in method
+//		- do it with creating a new temp array and a loop
+// - Print the elements of the reversed `aj`
+
+var aj = [3, 4, 5, 6, 7];
+var temp = [];
+for(var i = aj.length-1 ; i>=0; i--){
+    temp.push(aj[i]);
+
+}
+console.log(temp);
