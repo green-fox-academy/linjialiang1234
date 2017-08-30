23b57af22275d447cc040aef48dd5ab71b37f2f6
linjialiang1234 <linjialiang86@163.com>
2017-08-30 14:17:00 +0800

    update

--- week-02/day-03/exercise-15.js
+++ week-02/day-03/exercise-15.js
@@ -4,7 +4,12 @@
 // - Write a function called `sum` that sum all the numbers until the given parameter
 // - The function should return the result
 
+var result = 0;
 function sum(para){
-	return 
-
+	for(var i = 1; i<= para; i++){
+		result += i;
+	}
+	return result;
 }
+
+console.log(sum(5));
