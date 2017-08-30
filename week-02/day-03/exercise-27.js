b1d3b84f46939c8b5a71355af49846dc48129699
linjialiang1234 <linjialiang86@163.com>
2017-08-30 16:54:27 +0800

    update

--- week-02/day-03/exercise-27.js
+++ week-02/day-03/exercise-27.js
@@ -9,8 +9,14 @@
 // - In `colors[2]` store the shades of pink:
 //   `"orchid", "violet", "pink", "hot pink"`
 
-var colors = []
+var colors = [];
+colors[0] = [];
+colors[1] = [];
+colors[2] = [];
 
+colors[0].splice(0,"lime", "forest green", "olive", "pale green", "spring green");
+colors[1].splice(0,"orange red", "red", "tomato");
+colors[2].splice(0,"orchid", "violet", "pink", "hot pink");
 console.log(colors[0])
 console.log(colors[1])
 console.log(colors[2])
--- week-02/day-03/exercise-29.js
+++ week-02/day-03/exercise-29.js
@@ -12,3 +12,27 @@
 //
 // * size should depend on a variable
 // ** Relax, a matrix is just like an array
+var lineCount = 4;
+var matrix = [];
+for(var i = 0; i < lineCount; i++){
+    if(!matrix[i]) matrix[i] = [];
+    for(var j = 0; j < lineCount; j++){
+        if(j == (lineCount -1 - i)){
+            matrix[i].push(1);
+        }
+        else{
+            matrix[i].push(0);
+        }
+
+        // console.log(matrix[i][j]);
+        
+    }
+    var resultLine = [];
+    for(var j = 0; j < lineCount; j++){
+        resultLine.push(matrix[i][j]);
+    }
+    console.log(resultLine.join(" "));
+    
+
+
+}
