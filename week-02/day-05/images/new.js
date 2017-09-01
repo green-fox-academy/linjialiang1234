f67201425f7bfe9736120a7850de86ec687040b1
linjialiang1234 <linjialiang86@163.com>
2017-09-01 17:26:22 +0800

    update

---
+++ week-02/day-05/images/new.js
@@ -0,0 +1,15 @@
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("imageSingle");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
