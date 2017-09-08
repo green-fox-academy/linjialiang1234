24d5b2763a14fd44a9434d8af344ea67a6a305e2
linjialiang1234 <linjialiang86@163.com>
2017-09-08 10:05:52 +0800

    fix ajax post

--- week-03/day-04/reddit.js
+++ week-03/day-04/reddit.js
@@ -2,118 +2,97 @@ var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
   if (xhr.readyState == 4 && xhr.status == 200) {
-
     var result = JSON.parse(xhr.response);
     console.log(result.posts);
+    var getPostDiv = document.getElementsByClassName("post");
 
-     var getPostDiv = document.getElementsByClassName("post");
+    for (var i = 0; i < result.posts.length; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "singlePost";
+      getPostDiv[0].appendChild(newDiv);
+      var newDivInner = newDiv;
 
-    // var showId = document.getElementById("showId");
-    // var showTitle = document.getElementById("showTitle");
-    // var showHref = document.getElementById("showHref");
-    // var showScore = document.getElementById("showScore");
+      //first colum
+      createFirstColum(i);
     
+      //second colum
+      createSecondColum(i);
 
-    
-    for(var i = 0; i < result.posts.length; i++ ) {
+      //third colum
+      createThirdColum(i);
 
-        var newDiv = document.createElement("div");
-        newDiv.className = "singlePost";
-        getPostDiv[0].appendChild(newDiv);
-        var newDivInner = newDiv;
-        
+      // var newHref = document.createElement("div");
+      // newHref.innerHTML = result.posts[i].href;
+      // newHref.id = "showHref";
+    }
 
-        //first colum
-        var newId = document.createElement("div");
-        newId.innerHTML = result.posts[i].id;
-        newId.id = "showId";
-        newId.className = "colum";
-        newDiv.appendChild(newId);
-        
+    function createFirstColum(i){
+      var newId = document.createElement("div");
+      newId.innerHTML = result.posts[i].id;
+      newId.id = "showId";
+      newId.className = "colum";
+      newDiv.appendChild(newId);
 
-        //second colum
-        var newDivScore = document.createElement("div");
-        newDivScore.id = "secondColum";
-        newDivScore.className = "colum";
-        newDiv.appendChild(newDivScore);
+    }
 
-        var newUpArrow = document.createElement("img");
-        newUpArrow.src = "./images/upvote.png";
-        newDivScore.appendChild(newUpArrow);
-        var newScore = document.createElement("div");
-        newScore.innerHTML = result.posts[i].score;
-        newScore.id = "showScore";
-        newDivScore.appendChild(newScore);
-        var newDownArrow = document.createElement("img");
-        newDownArrow.src = "./images/downvote.png";
-        newDivScore.appendChild(newDownArrow);
-        
+    function createSecondColum(i){
+      var newDivScore = document.createElement("div");
+      newDivScore.id = "secondColum";
+      newDivScore.className = "colum";
+      newDiv.appendChild(newDivScore);
 
-        //third colum
+      var newUpArrow = document.createElement("img");
+      newUpArrow.src = "./images/upvote.png";
+      newDivScore.appendChild(newUpArrow);
+      var newScore = document.createElement("div");
+      newScore.innerHTML = result.posts[i].score;
+      newScore.id = "showScore";
+      newDivScore.appendChild(newScore);
+      var newDownArrow = document.createElement("img");
+      newDownArrow.src = "./images/downvote.png";
+      newDivScore.appendChild(newDownArrow);
+      }
+
+  function createThirdColum(i){
         var newDivTitle = document.createElement("div");
         newDivTitle.id = "thirdColum";
         newDivTitle.className = "colum";
         newDiv.appendChild(newDivTitle);
-        
+  
         var newTitle = document.createElement("div");
         newTitle.innerHTML = result.posts[i].title;
         newTitle.id = "showTitle";
         newDivTitle.appendChild(newTitle);
-
-
+  
+  
         var newTimeStamp = document.createElement("div");
         var newDate = new Date();
         newDate.setTime(result.posts[i].timestamp * 1000);
-       
-        newTimeStamp.innerHTML =  newDate.toDateString();
+  
+        newTimeStamp.innerHTML = newDate.toDateString();
         newTimeStamp.id = "showTimeStamp";
         newDivTitle.appendChild(newTimeStamp);
-
-
+  
+  
         var newOwner = document.createElement("div");
-        if(result.posts[i].owner === null){
+        if (result.posts[i].owner === null) {
           result.posts[i].owner = "anonymous";
         }
-
+  
         newOwner.innerHTML = result.posts[i].owner;
         newOwner.id = "showOwner";
         newDivTitle.appendChild(newOwner);
-
+  
         var newModifty = document.createElement("a");
         newModifty.innerHTML = "Modify";
         newModifty.id = "showModify";
         newDivTitle.appendChild(newModifty);
-
+  
         var newRemove = document.createElement("a");
         newRemove.innerHTML = "Remove";
         newRemove.id = "showRemovev";
         newDivTitle.appendChild(newRemove);
-
-
-
-
-        var newHref = document.createElement("div");
-        newHref.innerHTML = result.posts[i].href;
-        newHref.id = "showHref";
-        
-        
-       
-
-
-
-        // newDiv.appendChild(newTitle);
-        // newDiv.appendChild(newHref);
-        // newDiv.appendChild(newTimeStamp);
-        // newDiv.appendChild(newOwner);
-        
-
-
-        
-        
-        // showId.innerHTML = result.posts[i].id;
-
-    }
-
+      }
   }
 
 }
