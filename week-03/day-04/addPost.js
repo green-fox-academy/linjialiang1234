4f5dde2bc7c6f8df453a6d95d95061283a792458
linjialiang1234 <linjialiang86@163.com>
2017-09-11 10:58:02 +0800

    update

---
+++ week-03/day-04/addPost.js
@@ -0,0 +1,38 @@
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function () {
+  checkInputEmpty();
+});
+
+function checkInputEmpty() {
+  var urlValue = document.getElementById("urlInput");
+  var titleValue = document.getElementById("titleInput");
+  if (urlValue.value === "") {
+    alert("Please input the URL!");
+    return;
+  }
+  if (titleValue.value === "") {
+    alert("Please input the title!");
+    return;
+  }
+  postData(urlValue,titleValue);
+}
+
+function postData(urlValue, titleValue) {
+  var xhr = new XMLHttpRequest();
+  var postData = {
+      "title": titleValue.value,
+      "href": urlValue.value
+  };
+  var jSONPostData = JSON.stringify(postData);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var result = JSON.parse(xhr.responseText);
+      console.log("success receviced data: " + result);
+      window.history.back();
+    }
+  }
+  xhr.open("POST", "https://time-radish.glitch.me/posts", true);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send(jSONPostData);
+}
---
+++ week-03/day-04/modify.js
@@ -0,0 +1,44 @@
+var urlValue = document.getElementById("urlInput");
+var titleValue = document.getElementById("titleInput");
+urlValue.value = localStorage.getItem("url");
+titleValue.value = localStorage.getItem("title");
+
+var idValue = localStorage.getItem("id");
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function () {
+  checkInputEmpty();
+});
+
+function checkInputEmpty() {
+  if (urlValue.value === "") {
+    alert("Please input the URL!");
+    return;
+  }
+  if (titleValue.value === "") {
+    alert("Please input the title!");
+    return;
+  }
+  console.log("urlValue: " + urlValue.value + "titleValue: " + titleValue.value);
+  putData(urlValue,titleValue);
+}
+
+function putData(urlValue, titleValue) {
+  var xhr = new XMLHttpRequest();
+  var postData = {
+      "title": titleValue.value,
+      "href": urlValue.value
+    };
+  var jSONPostData = JSON.stringify(postData);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var result = JSON.parse(xhr.responseText);
+      console.log("success UPDATE data: " + result);
+      window.location.href = "./reddit.html";
+    }
+  }
+  xhr.open("PUT", "https://time-radish.glitch.me/posts/"+idValue, true);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-type", "application/json");  
+  xhr.send(jSONPostData);
+}
