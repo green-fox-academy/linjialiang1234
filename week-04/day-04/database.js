9c65ee359ff2c9e1be2ff35c2aedcf3a797573f2
linjialiang1234 <linjialiang86@163.com>
2017-09-18 08:22:30 +0800

    change get and post

---
+++ week-04/day-04/database.js
@@ -0,0 +1,71 @@
+'use strict'
+
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017/epam';
+var defaultData = [{"title":"Dear JavaScript","href":"http://9gag.com","id": 1,"timestamp":1494339525999,"score":2,"owner":null, "vote": 1},
+{"title":"Post 1","href":"http://9gag.com","id":2,"timestamp":1505456041859,"score":-1,"owner":"kristof4", "vote": -1},
+{"title":"Nothing special","href":"http://9gag.com","id":3,"timestamp":1500000000000,"score":0,"owner":"kristof4", "vote":0}];
+
+var count = 0;
+
+var obj = {
+  posts: []
+};
+function createDB(){
+  MongoClient.connect(url,function(err, db){
+      if (err) {
+          console.log('Unable to connect to the MongoDB server. Error:', err);
+      }
+      console.log('Connection established to ' + url);
+
+      // if(!db.collection("reddit")){
+      //     db.createCollection("reddit", function(err, res) {
+      //         if (err) throw err;
+      //       });
+      // }
+
+      var redditDB = db.collection("reddit");
+      // redditDB.remove();
+      redditDB.insertMany(defaultData, function(err, res){
+         // console.log(res.insertedCount);
+      });
+      db.close();
+  }); 
+}
+
+function showData(callback) {
+  MongoClient.connect(url, function(err,db) {
+    var redditDB = db.collection("reddit");
+    redditDB.find({}).toArray(function(err, result) {
+      if(err) throw err;
+      count = result.length;
+      obj.posts = result;
+      db.close();
+      callback(obj);
+    })
+  })
+}
+
+function createPost(body,username,callback){
+  MongoClient.connect(url, function(err,db) {
+    var redditDB = db.collection("reddit");
+
+    body.score = 0;
+    body.vote = 0;
+    body.timestamp = new Date().getTime();
+    body.owner = username || null;
+
+    redditDB.insertOne(body, function(err,res) {
+      callback(body);
+    });
+    db.close();
+  })
+
+}
+module.exports = {
+  createDB : createDB,
+  showData : showData,
+  createPost : createPost
+}
---
+++ week-04/day-04/test.js
@@ -0,0 +1,302 @@
+'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var app = express();
+// app.set('view engine', 'html');
+
+app.use('/', express.static('public'));
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/reddit';
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  var collection = db.collection('students');
+ 
+  collection.find({}).toArray(function (err, docs) {
+    console.dir(docs);
+
+  });
+  db.close();
+});
+
+app.get('/hello', function (req, res) {
+  res.send('hello world');
+})
+app.listen(8080);
+
+//lists all the posts
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/public/reddit.html");
+})
+
+
+
+
+app.get('/posts', function (req, res) {
+  var obj = {
+    posts: []
+  };
+
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+    collection.find({}).toArray(function (err, docs) {
+      obj.posts = docs;
+      res.send(obj);
+    });
+    db.close();
+  })
+});
+
+
+app.post('/posts', jsonParser, function (req, res) {
+  var obj = {
+    posts: []
+  };
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+
+    collection.findOne({}, {sort: {id: -1}}, function (err, result) {
+      console.log(result.id);
+      var updateId = parseInt(result.id) + 1;
+      console.log("newObjid:" + updateId);
+
+      var newObj = {
+        'title': req.body.title,
+        'href': req.body.href,
+        'id': updateId,
+        'timestamp': new Date().getTime(),
+        'score': 0,
+        "owner": "anonymous"
+      }
+
+      collection.insertOne(newObj, function (err, docs) {
+        if (err) throw err;
+        res.setHeader("Content-Type", "application/json");
+        res.send(JSON.stringify(docs));
+        console.log(docs + " has inserted")
+        db.close();
+      });
+    });
+
+
+
+  });
+
+
+});
+
+app.post('/login', jsonParser, function (req, res) {
+  var obj = {
+    posts: []
+  };
+  // var updateId = 0;
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+
+    var loginOwner = req.body.owner;
+
+    console.log("reqbody,owb " + loginOwner);
+
+    // collection.insertOne(newObj,function(err,docs) {
+
+      // collection.find({}).toArray(function (err, docs) {
+      //   obj.posts = docs;
+      //   res.send(obj);
+      // });
+
+
+    collection.find({
+      owner: loginOwner
+    }).toArray(function (err, result) {
+
+      res.setHeader("Content-Type", "application/json");
+     
+      // console.log("00000000 " + result[0]);
+      console.log("111" + result[0].owner);
+   
+      console.dir(result);
+
+      res.send(JSON.stringify(result));
+
+      db.close();
+    });
+
+
+  })
+
+})
+
+app.put('/posts/:id', jsonParser, function (req, res) {
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+    var collection = db.collection('students');
+    var queryId = parseInt(req.params.id);
+    var modifyTitle = req.body.title;
+    var modifyHref = req.body.href;
+
+    collection.update({
+      id: queryId
+    }, {
+      $set: {
+        'title': modifyTitle,
+        'href': modifyHref
+      }
+    }, function (err, result) {
+      collection.find({}).toArray(function (err, docs) {
+        res.setHeader("Content-Type", "application/json");
+        res.send(JSON.stringify(docs));
+        db.close();
+      })
+    });
+  })
+
+
+});
+
+app.put('/posts/:id/upvote/:score', jsonParser, function (req, res) {
+  var obj = {
+    posts: []
+  };
+  var upvoteId = req.params.id;
+  var upvoteScore = req.params.score;
+ 
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+
+    var newObj = {
+      "id": upvoteId,
+      "score": upvoteScore,
+      "vote": 1
+    };
+
+    var idValue = parseInt(newObj.id);
+    var scoreValue = parseInt(newObj.score) + 1;
+
+    var myquery = {
+      id: newObj.id
+    };
+    var newvalues = {
+      $set: {
+        score: newObj.score
+      }
+    };
+    collection.updateMany({id: idValue}, {$set: {'score': scoreValue,'vote': 1}}, function (err, res) {
+      if (err) throw err;
+      console.log("1111 score document updated");
+    });
+
+    collection.find({}).toArray(function (err, docs) {
+      // console.dir(docs);
+      obj.posts = docs;
+      res.send(obj);
+    });
+
+    db.close();
+  });
+});
+
+app.put('/posts/:id/downvote/:score', jsonParser, function (req, res) {
+  var obj = {
+    posts: []
+  };
+  var upvoteId = req.params.id;
+  var upvoteScore = req.params.score;
+  // upvoteScore += 1;
+  console.log("123: " + req.params.id)
+  console.log("234: " + req.params.score);
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+
+    var newObj = {
+      "id": upvoteId,
+      "score": upvoteScore,
+      "vote": -1
+    };
+
+    var idValue = parseInt(newObj.id);
+    var scoreValue = parseInt(newObj.score) - 1;
+
+    var myquery = {
+      id: newObj.id
+    };
+    var newvalues = {
+      $set: {
+        score: newObj.score
+      }
+    };
+    collection.updateMany({
+      id: idValue
+    }, {
+      $set: {
+        'score': scoreValue,
+        'vote': -1
+      }
+    }, function (err, res) {
+      if (err) throw err;
+      console.log("1111 score document updated");
+    });
+
+    collection.find({}).toArray(function (err, docs) {
+      // console.dir(docs);
+      obj.posts = docs;
+      res.send(obj);
+    });
+
+    db.close();
+  });
+
+
+});
+
+app.delete('/posts/:id', function (req, res) {
+  var obj = {
+    posts: []
+  };
+  var deletedId = parseInt(req.params.id);
+  console.log("deleted Id " + deletedId);
+  MongoClient.connect(url, function (err, db) {
+    var collection = db.collection('students');
+    if (err) {
+      console.log('Unable to connect to the MongoDB server. Error:', err);
+    }
+
+    collection.remove({
+      id: deletedId
+    }, function (err, result) {
+      console.log(result);
+    });
+
+    collection.find({}).toArray(function (err, docs) {
+      // console.dir(docs);
+      obj.posts = docs;
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(obj));
+      // res.send(obj);
+    });
+    db.close();
+
+  });
+
+})
