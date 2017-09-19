e18e7a6d99ed2d264edeb582bfc3737b56b8d8ec
linjialiang1234 <linjialiang86@163.com>
2017-09-19 18:07:05 +0800

    left button

--- week-05/day-02/exercise-02/src/app/index.js
+++ week-05/day-02/exercise-02/src/app/index.js
@@ -1,17 +1,41 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
 var App = React.createClass({
+  render:function() {
+    return (
+      <div>
+      <Viewer/>
+      <ThumbnailComponent/>
+      </div>
+    );
+  }
+})
+var Viewer = React.createClass({
   render:function(){
     return (
-    <div>
+    <div className="viewer">
+      <LeftArrow/>
       <MainImage/>
-      <ThumbnailComponent/>
-
+      <RightArrow/>
     </div>
     )
   }
 })
+
+var LeftArrow = React.createClass({
+  render:function(){
+    return(
+      <div className="leftarrow">
+       <a onClick="plusSlides(-1)"><img className="leftArrowImage" src="leftarrow.svg" /></a>
+    </div>
+
+    );
+  }
+
+
+
+
+});
 var MainImage = React.createClass({
   getInitialState:function() {
     return {
@@ -39,21 +63,63 @@ var MainImage = React.createClass({
   },
  
   render:function(){
+      var imagesArray = this.state.imageInformation;
+      imagesArray = imagesArray.map(function(item,index) {
+        return (
+          <div className="images">
+          <SingleImage item={item} key={index}/>
+          </div>
+        );
+      }.bind(this));
       return (
-        <div id="images">
-        <div className="imageSingle">
-          <div className="photo">
-          <img src= {this.state.imageInformation[0].imageSrc}/>
-          </div>
-          <div className="description">
-            <h3>{this.state.imageInformation[0].title}</h3>
-            <p>{this.state.imageInformation[0].description}</p>
-          </div>
-        </div>
+        <div>
+          {imagesArray}
         </div>
       )
     }
  
+});
+
+var SingleImage = React.createClass({
+  render:function() {
+    return (
+      // <div id="images">
+        <div className="imageSingle">
+          <div className="photo">
+           <img src= {this.props.item.imageSrc}/>
+          </div>
+          <div className="description">
+             <h3>{this.props.item.title}</h3>
+             <p>{this.props.item.description}</p>
+          </div>
+        </div>
+          
+    );
+  }
+})
+
+var RightArrow = React.createClass({
+  render:function(){
+    return (
+
+      <div className="rightarrow">
+      <a onClick={this.plusSlides}><img className="rightArrowImage" src="leftarrow.svg"/>></a>
+    </div>
+    )
+  },
+
+  plusSlide: function() {
+    var updateImages = this.state.imageInformation
+    var changeIndexArray = []
+    for(var i = 0; i < updateImages.length; i++) {
+      updateImages[i] = updateImages[i+1];
+      changeIndexArray.push(updateImages[i]);
+    }
+    this.setState({
+      imageInformation : changeIndexArray
+    });
+  }
+
 });
 
 var ThumbnailComponent = React.createClass({
@@ -85,11 +151,11 @@ var ThumbnailComponent = React.createClass({
   render:function() {
     return (
       <div className="thumbnails">
-        <a className="dot"><span className="tooltiptext">Cat</span><img src={this.state.imageInformation[0].imageSrc}/>></a>
-        <a className="dot"><span className="tooltiptext">Goat</span><img src={this.state.imageInformation[1].imageSrc}/>></a>
-        <a className="dot"><span className="tooltiptext">Lion</span><img src={this.state.imageInformation[2].imageSrc}/>></a>
-        <a className="dot"><span className="tooltiptext">Mouse</span><img src={this.state.imageInformation[3].imageSrc}/></a>
-        <a className="dot"><span className="tooltiptext">Squiral</span><img src={this.state.imageInformation[4].imageSrc}/></a>
+        <a className="dot" onClick="currentSlide(1)"><span className="tooltiptext">Cat</span><img src={this.state.imageInformation[0].imageSrc}/>></a>
+        <a className="dot" onClick="currentSlide(2)"><span className="tooltiptext">Goat</span><img src={this.state.imageInformation[1].imageSrc}/>></a>
+        <a className="dot" onClick="currentSlide(3)"><span className="tooltiptext">Lion</span><img src={this.state.imageInformation[2].imageSrc}/>></a>
+        <a className="dot" onClick="currentSlide(4)"><span className="tooltiptext">Mouse</span><img src={this.state.imageInformation[3].imageSrc}/></a>
+        <a className="dot" onClick="currentSlide(5)"><span className="tooltiptext">Squiral</span><img src={this.state.imageInformation[4].imageSrc}/></a>
       </div>
     );
   }
@@ -97,4 +163,3 @@ var ThumbnailComponent = React.createClass({
 });
 
 ReactDOM.render(<App />,document.getElementById("bodyContainer"));
-// ReactDOM.render(<ThumbnailComponent />,document.getElementsByClassName("thumbnailsContainer"));
