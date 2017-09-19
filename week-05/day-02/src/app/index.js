7986f97a48942de939ca19be21e58f05ea67e598
linjialiang1234 <linjialiang86@163.com>
2017-09-19 11:59:26 +0800

    React props

--- week-05/day-02/src/app/index.js
+++ week-05/day-02/src/app/index.js
@@ -5,9 +5,15 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   render:function(){
     return (
-        <h1>Ninjia</h1>
+        <div>
+          <p><strong>Cheese name: </strong>{this.props.cheese.name}</p>
+          <p><strong>Cheese small factor: </strong>{this.props.cheese.smellFactor}</p>
+          <p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
+        </div>
     );
   }
 });
+
+var myCheese = {name: "Camembert", smellFactor:"Extreme pong",price:"3.50"};
 //put component into html page
-ReactDOM.render(<TodoComponent/>,document.getElementById("todo-wrapper"))
+ReactDOM.render(<TodoComponent mssg="I like cheese" cheese={myCheese}/>,document.getElementById("todo-wrapper"))
