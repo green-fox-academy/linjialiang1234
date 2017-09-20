edd807a29e4ae88b7aaf4ab61368bb79ffd22d75
linjialiang1234 <linjialiang86@163.com>
2017-09-20 20:25:53 +0800

    Sass

--- week-05/day-03/webpack-demo/src/index.js
+++ week-05/day-03/webpack-demo/src/index.js
@@ -1,20 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg'
+import printMe from './print.js'
+
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  //Add the image to our existing div
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
--- week-05/day-03/webpack-demo/webpack.config.js
+++ week-05/day-03/webpack-demo/webpack.config.js
@@ -1,26 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
-  module: {
-    rules: [
-
-      {
-        test: /\.css$/,
-        use:['style-loader','css-loader']
-      },
-
-      {
-        test:/\.(png|svg|jpg|gif)$/,
-        use:['file-loader']
-      }
-    
-    ]
-
-
+   devtool: 'inline-source-map',
+   devServer: {
+        contentBase: './dist'
+       },
+  plugins: [
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
