f2de1ab3ed7f765d7f875f3adbc77602cd11ea77
linjialiang1234 <linjialiang86@163.com>
2017-09-22 12:02:52 +0800

    fix layout

--- week-05/test/github.js
+++ week-05/test/github.js
@@ -1,19 +1,85 @@
 'use strict'
-var url = "https://github.com/";
+const url = "https://api.github.com/";
 // GET /repos/:owner/:repo
 //https://api.github.com/repos/greenfox-academy/linjialiang1234/commits
 
 //151c9ddd42be0d62891f785cc61745669742500f ;
 //https://api.github.com/rate_limit
+
+//"https://api.github.com/search/repositories?q=topic:epam-jsa"
 const OWNER = "linjialiang1234";
 var repo = "";
+window.addEventListener('load', init);
+//init go button, login button and recommend list
+function init() {
+  //initial serach button
+  var searchButton = document.getElementsByClassName("search-button")[0];
+  searchButton.addEventListener("click", searchRepository);
 
-var valueOfSeach = document.getElementsByClassName("search-bar")[0].value;
-var searchButton = document.getElementsByClassName("search-button")[0];
-searchButton.addEventListener("click", searchRepository);
+  //initial login button
+  var loginButton = document.getElementsByClassName("login-button")[0];
+  loginButton.addEventListener("click", onAuthentication);
 
-var loginButton = document.getElementsByClassName("login-button")[0];
-loginButton.addEventListener("click", onAuthentication);
+  //initial recommended list
+  displayRecommended();
+}
+
+function displayRecommended() {
+  fetch("https://api.github.com/search/repositories?q=topic:epam-jsa").then(function (response) {
+    console.log(response.status);
+    if (response.status === 404) {
+      document.getElementsByClassName("search-bar")[0].setAttribute("placeholder", "Not Found");
+    } else {
+      // console.log(response.headers);
+      response.json().then(function (data) {
+        console.log(data);
+        displaySingleRecommended(data);
+      })
+    }
+  })
+}
+
+function displaySingleRecommended(data) {
+  var recommendedContent = document.getElementsByClassName("recommended-content")[0];
+  for (var i = 0; i < data.items.length; i++) {
+    var singleName = document.createElement("a");
+    singleName.innerHTML = data.items[i].name;
+    singleName.className = data.items[i].name;
+    singleName.addEventListener("click", function () {
+      searchNameRepository(singleName.className)
+    });
+    recommendedContent.appendChild(singleName);
+  }
+}
+
+function searchNameRepository(searchName) {
+  var newSearchValue = document.getElementsByClassName(searchName);
+  console.log(newSearchValue);
+  searchRepository(newSearchValue);
+}
+
+function searchRepository() {
+  var valueOfSeach = document.getElementsByClassName("search-bar")[0].value;
+  repo = valueOfSeach;
+  //"repos/" + OWNER + "/" + repo
+  console.log("repo: " + repo);
+  fetch(url + "repos/greenfox-academy/" + repo).then(function (response) {
+    console.log(response.status);
+    if (response.status === 404) {
+      // alert("Not found");
+      document.getElementsByClassName("search-bar")[0].setAttribute("placeholder", "Not Found");
+    } else {
+      // console.log(response.headers);
+      response.json().then(function (data) {
+       
+        console.log(data);
+        displayInformation(data);
+        displayCommitMessage(data);
+      })
+    }
+
+  })
+}
 
 function onAuthentication() {
   var userName = document.getElementsByClassName("user-name")[0].value;
@@ -26,129 +92,40 @@ function onAuthentication() {
     method: 'GET',
     headers: myHeaders
   }
-  fetch("https://api.github.com/rate_limit",myInit).then(function(response) {
-    // console.log(response.json().then(function(body) {
-    //   console.log(body);
-    // }));
-
-    response.json().then(function(body) {
-     console.log(body.rate.limit);
+  fetch("https://api.github.com/rate_limit", myInit).then(function (response) {
+    response.json().then(function (body) {
+      console.log(body.rate.limit);
     });
-    // if(response.status === 404) {
-    //   // alert("Not found");
-    //   document.getElementsByClassName("search-bar")[0].setAttribute("placeholder", "Not Found");
-    // } else {
-    // console.log(response.headers);
-
-
-    // response.json().then(function(data) {
-    //   // messagesKeeper = data.messages;
-    //   // loadPage(data.messages);
-    //   console.log(data);
-    //   displayInformation(data);
-    //   displayCommitMessage(data);
-    //   displayRecommended(data);
-    //   //create 404 alert
-
-    //   //display commit message
-
-    // })
-
-
-  // }
-
   })
-
-
-
-
-}
-
-
-function searchRepository(valueOfSeach){
-  repo = valueOfSeach;
-//"repos/" + OWNER + "/" + repo
-  fetch("https://api.github.com/search/repositories?q=topic:epam-jsa").then(function(response) {
-    console.log(response.status);
-    if(response.status === 404) {
-      // alert("Not found");
-      document.getElementsByClassName("search-bar")[0].setAttribute("placeholder", "Not Found");
-    } else {
-    // console.log(response.headers);
-    response.json().then(function(data) {
-      // messagesKeeper = data.messages;
-      // loadPage(data.messages);
-      console.log(data);
-      displayInformation(data);
-      displayCommitMessage(data);
-      displayRecommended(data);
-      //create 404 alert
-
-      //display commit message
-
-    })
-  }
-
-  })
-
 }
 
 function displayInformation(data) {
   document.getElementsByClassName("repository-name")[0].innerHTML = data.name;
   document.getElementsByClassName("repository-description")[0].innerHTML = data.description;
   document.getElementsByClassName("repository-last-commit")[0].innerHTML = "Last updated at " + data.created_at;
-
 }
 
 function displayCommitMessage(data) {
-  var commitsContent = document.getElementsByClassName("commits-content")[0];
-  for(var i = 0; i < data.length; i++) {
-    var singleCommitMessage = document.createElement("div");
-    singleCommitMessage.innerHTML = data[i].commit.message;
-    commitsContent.appendChild(singleCommitMessage);
-    var signleCommitterAndTime = document.createElement("div");
-    signleCommitterAndTime.innerHTML = data[i].commit.committer.name + " at " + data[i].commit.committer.date;
-    commitsContent.appendChild(signleCommitterAndTime);
-  }
-}
 
-function displayRecommended(data) {
-  console.log(data.items[0].name);
-  var recommendedContent= document.getElementsByClassName("recommended-content")[0];
-  for(var i = 0; i < data.items.length; i++){
-  var singleName = document.createElement("a");
-  singleName.innerHTML = data.items[i].name;
-  singleName.className = data.items[i].name;
-  singleName.addEventListener("click", function(){searchNameRepository(singleName.className)});
-  recommendedContent.appendChild(singleName);
+  fetch("https://api.github.com/repos/greenfox-academy/linjialiang1234/commits").then(function (response) {
+    response.json().then(function (data) {
+      
+       console.log("commit message: " +data);
+       var commitsContent = document.getElementsByClassName("commits-content")[0];
+       for (var i = 0; i < data.length; i++) {
+         var singleCommitMessage = document.createElement("div");
+         singleCommitMessage.innerHTML = data[i].commit.message;
+         commitsContent.appendChild(singleCommitMessage);
+         var signleCommitterAndTime = document.createElement("div");
+         signleCommitterAndTime.innerHTML = data[i].commit.committer.name + " at " + data[i].commit.committer.date;
+         commitsContent.appendChild(signleCommitterAndTime);
+       }
+       // displayInformation(data);
+      //  displayCommitMessage(data);
+     })
   
-   }
-}
+  
+  })
 
-function searchNameRepository(searchName) {
-  // var searchName = data.items[i].name;
-  var newSearchValue = document.getElementsByClassName(searchName);
-  console.log(newSearchValue);
-  searchRepository(newSearchValue);
-}
-// init();
-// function init() {
-//   fetch(url + "repos/" + OWNER + "/repo").then(function(response) {
-//     response.json().then(function(data) {
-//       messagesKeeper = data.messages;
-//       loadPage(data.messages);
-//     })
-//   })
-//   var submit = document.getElementById('submit');
-//   submit.addEventListener('click', postInfo);
-// }
 
-// function loadPage(data) {
-//   container.innerHTML = '';
-//   data.forEach(function(value) {
-//     var message = document.createElement('span');
-//     message.innerText = `${value.name} : ${value.message}`;
-//     container.appendChild(message);
-//   }, this);
-//   container.scrollTop = container.scrollHeight;
-// }
+}
